3d5869dbe395b68b8433cb962c2ec66828554a87
Omkar Garate <garateomkar143@gmail.com>
2024-03-31 05:18:16 +0530

    added leaderboard logic

--- webwarriors/src/Components/LeaderBoard.js
+++ webwarriors/src/Components/LeaderBoard.js
@@ -3,8 +3,9 @@ import bglb from '../Images/bglb.jpg'
 import gold from '../Images/gold.png'
 import silver from '../Images/silver.png'
 import bronze from '../Images/bronze.png'
-import user from '../Images/user.png'
-import sparkle from '../Images/sparkle.png'
+import userProf from '../Images/user.png'
+import "../css/leaderboard.css"
+import { useAuthContext } from '../hooks/useAuthContext'
 
 function LeaderBoard() {
     const { user } = useAuthContext();
@@ -48,46 +49,19 @@ function LeaderBoard() {
             <h1>LEADERBOARD</h1>
             <h2></h2>
             <div className="lbContent">
-            <div className="lbC1">
-                <img src={gold} alt="" />
-                <img src={user} alt="" />
-                <div className="uname">
-                    <p>UserName</p>
-                    <p>1259</p>
-                </div>
-            </div>
-            <div className="lbC1">
-                <img src={silver} alt="" />
-                <img src={user} alt="" />
-                <div className="uname">
-                    <p>UserName</p>
-                    <p>1259</p>
-                </div>
-            </div>
-            <div className="lbC1">
-                <img src={bronze} alt="" />
-                <img src={user} alt="" />
-                <div className="uname">
-                    <p>UserName</p>
-                    <p>1259</p>
-                </div>
-            </div>
-            <div className="lbC1">
-                <p  className='rank'>4</p>
-                <img src={user} alt="" />
-                <div className="uname">
-                    <p>UserName</p>
-                    <p>1259</p>
-                </div>
-            </div>
-            <div className="lbC1">
-                <p  className='rank'>5</p>
-                <img src={user} alt="" />
-                <div className="uname">
-                    <p>UserName</p>
-                    <p>1259</p>
-                </div>
-            </div>
+                {events.map((eve, index)=>(
+                    <div className="lbC1" key={index}>
+                        <img src={gold} alt="" />
+                        {/* <img src={userProf} alt="" /> */}
+                        <img src={`http://localhost:5001/uploads/${eve.userProfile}`} alt="profileImg"/>
+                        <div className="uname">
+                            <p>{eve.username}</p>
+                            <p>{eve.registered}</p>
+                        </div>
+                    </div>
+                ))}
+            
+           
             
         </div>
         </div>
